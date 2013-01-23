046063fd28c7c2a6cbb010d177e641ecb01df795
Dustin Quam <quamsta@gmail.com>
2013-01-23 09:35:57 -0600

    fixed sorting, hopefully for good.

--- wp-content/themes/bijou/category-coming-soon.php
+++ wp-content/themes/bijou/category-coming-soon.php
@@ -17,23 +17,50 @@ get_header(); ?>
 
 	<ul>
 			<?php
-				$dataQuery = new WP_Query( array(
-					'post_type' => 'post',
-					'category_name' => 'coming-soon',
-					'posts_per_page' => 20, // get 10 posts
-					'paged' => get_query_var( 'page' ), 
-					'orderby' => 'meta_value_num',
-					'meta_key' => 'film_start_date',
-					'order'	=>	'ASC'
-				));
-				
-				//print_r($dataQuery);
-			?>
 			
+			
+			$agenda = get_posts(
+			
+					array('post_type' => 'post',
+					'category_name' => 'coming-soon',
+					'posts_per_page' => 30, // get 20 posts
+					'paged' => get_query_var( 'page' )
+					)		
+			
+			
+			);
 
+// an empty array
+$date_order = '';
+if( $agenda ) : ?> <?php  foreach( $agenda as $post ) : setup_postdata( $post ); ?> 
+
+<?php 	$custom_date = get_post_custom_values('EventStartDate (YYYY/MM/DD)');
+	if (isset($custom_date[0])) {
 	
-	<?php if ($dataQuery->have_posts() ) : while ( $dataQuery->have_posts() ) : $dataQuery->the_post(); ?>
+		$evento = strtotime($custom_date[0]);
+		
+	} else {
+   $evento = strtotime($post->post_date_gmt);
+			}
+	$date_order[$evento] = $post->ID;
 
+// now the array contain key as date and post ID as value
+	 ?>
+
+	<?php endforeach;  ?>
+ 	<?php endif; ?>
+<?php		
+ksort($date_order);
+
+//print_r($date_order);
+
+foreach ($date_order as $key => $val) {
+	query_posts('p='.$val);
+	global $more;
+	$more = 0; 
+	
+	// the second Loop
+	while (have_posts()) : the_post(); ?>
 		<li>
 			<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 		
@@ -61,7 +88,49 @@ get_header(); ?>
 	<div style="clear: left;"></div>
 	</li>
 	<div style="clear: left;"></div>
-<?
+
+
+ <?php	} 
+	 
+	 print paginate_links( $pagination ) ?>
+	 
+ 	
+			
+			<?php /*
+								$start_date = get_post_meta($post->ID, 'EventStartdate (YYYY/MM/DD)', true);
+					$fixed_date = strtotime($start_date);
+			
+				$dataQuery = new WP_Query( array( 
+					'post_type' => 'post',
+					'category_name' => 'coming-soon',
+					'meta_key' => 'EventStartDate (YYYY/MM/DD)',
+					'orderby' => $fixed_date,
+					'order'	=>	'ASC',
+					'posts_per_page' => 20, // get 20 posts
+					'paged' => get_query_var( 'page' )
+				));
+				
+				//print_r($dataQuery);
+			?>
+			
+
+	
+	<?php /* if ($dataQuery->have_posts() ) : while ( $dataQuery->have_posts() ) : $dataQuery->the_post(); ?>
+
+		<li>
+			<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+		
+<div class="schedule_container">
+			<?php
+			 
+			 echo ec3_get_schedule('<tr><td colspan="3">%s</td></tr>', '<tr><td class="ec3_start">%1$s</td>'
+			  . '<td class="ec3_to">%3$s</td><td class="ec3_end">%2$s</td></tr>','<table class="ec3_schedule" cellpadding=10>%s</table>');
+			
+			
+			
+
+<?php
+/*
 global $wp_rewrite;
 $dataQuery->query_vars['paged'] > 1 ? $current = $dataQuery->query_vars['paged'] : $current = 1;
  
@@ -72,7 +141,7 @@ $pagination = array(
 	'current' => $current,
 	'show_all' => true,
 	'type' => 'plain',
-	/*'prev_text' => '', */
+	//'prev_text' => '', 
 	'next_text' => '&nbsp; &nbsp; Future Shows &raquo;'
 	);
 	
@@ -90,7 +159,8 @@ print paginate_links( $pagination ) ?>
 		<p class="center">Sorry, but you are looking for something that isn't here.</p>
 		<?php get_search_form(); ?>
 
-<?php endif; ?>
+<?php endif; ?> */
+?>
 </ul>		
 
 </div>
